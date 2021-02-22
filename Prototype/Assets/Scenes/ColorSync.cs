e95f452844ead7c831074cdf4bf10c6e338f1baa
Snaylo <42110788+ScottJamesNaylor@users.noreply.github.com>
2021-02-22 14:47:48 +0100

    Scene with drum sfx and synched data scripts

---
+++ Prototype/Assets/Scenes/ColorSync.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class ColorSync : RealtimeComponent<ColorSyncModel> {
+    private MeshRenderer   _meshRenderer;
+
+    private void Awake() {
+        // Get a reference to the mesh renderer
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    protected override void OnRealtimeModelReplaced(ColorSyncModel previousModel, ColorSyncModel currentModel) {
+        if (previousModel != null) {
+            // Unregister from events
+            previousModel.colorDidChange -= ColorDidChange;
+        }
+
+        if (currentModel != null) {
+            // If this is a model that has no data set on it, populate it with the current mesh renderer color.
+            if (currentModel.isFreshModel)
+                currentModel.color = _meshRenderer.material.color;
+
+            // Update the mesh render to match the new model
+            UpdateMeshRendererColor();
+
+            // Register for events so we'll know if the color changes later
+            currentModel.colorDidChange += ColorDidChange;
+        }
+    }
+
+    private void ColorDidChange(ColorSyncModel model, Color value) {
+        // Update the mesh renderer
+        UpdateMeshRendererColor();
+    }
+
+    private void UpdateMeshRendererColor() {
+        // Get the color from the model and set it on the mesh renderer.
+        _meshRenderer.material.color = model.color;
+    }
+
+    public void SetColor(Color color) {
+        // Set the color on the model
+        // This will fire the colorChanged event on the model, which will update the renderer for both the local player and all remote players.
+        model.color = color;
+    }
+}
---
+++ Prototype/Assets/Scenes/ColorSyncTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSyncTest : MonoBehaviour {
+    [SerializeField]
+    private Color _color         = default;
+    private Color _previousColor = default;
+
+    private ColorSync _colorSync;
+
+    private void Awake() {
+        // Get a reference to the color sync component
+        _colorSync = GetComponent<ColorSync>();
+    }
+
+    private void Update() {
+        // If the color has changed (via the inspector), call SetColor on the color sync component.
+        if (_color != _previousColor) {
+            _colorSync.SetColor(_color);
+            _previousColor = _color;
+        }
+    }
+}

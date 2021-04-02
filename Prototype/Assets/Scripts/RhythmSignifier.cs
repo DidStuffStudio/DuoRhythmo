0399c131a9e388fa1fd70e5fc9168a5af368d838
Snaylo <42110788+ScottJamesNaylor@users.noreply.github.com>
2021-04-02 11:56:30 +0200

    Changed wait time on rhythm signifier

--- Prototype/Assets/Scripts/RhythmSignifier.cs
+++ Prototype/Assets/Scripts/RhythmSignifier.cs
@@ -8,8 +8,18 @@ public class RhythmSignifier : MonoBehaviour {
         if (other.gameObject.layer != 6) yield return null;
         var node = other.transform.GetComponentInParent<Node>();
         node.PlayDrum();
+        var nodeRT = node.GetComponent<RectTransform>();
+        nodeRT.position = Vector3.Lerp( nodeRT.position,new Vector3(nodeRT.position.x,nodeRT.position.y, nodeRT.position.z+10.0f), 0.1f);
         node.canPlay = false;
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.01f);
         node.canPlay = true;
+    }
+
+    IEnumerator  OnTriggerExit(Collider other)
+    {
+        var node = other.GetComponentInParent<Node>();
+        var nodeRT = node.GetComponent<RectTransform>();
+        nodeRT.position = Vector3.Lerp( nodeRT.position,new Vector3(nodeRT.position.x,nodeRT.position.y, nodeRT.position.z-10.0f), 0.1f);
+        yield return new WaitForSeconds(0.01f);
     }
 }

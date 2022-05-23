bc22b2e1c11f96e3feb1f47c80d0545e26b403ce
Snaylo <42110788+ScottJamesNaylor@users.noreply.github.com>
2022-05-23 14:43:58 +0200

    Some crappy attempt at the line renderer for the pin

--- Prototype/Assets/Scripts/Custom Buttons/Did Stuff Buttons/Buttons/PasswordButton.cs
+++ Prototype/Assets/Scripts/Custom Buttons/Did Stuff Buttons/Buttons/PasswordButton.cs
@@ -7,22 +7,53 @@ namespace Custom_Buttons.Did_Stuff_Buttons.Buttons
   {
     private PinManager _pinManager;
     [SerializeField] private int _value;
-    private Vector2 _position;
+    private Vector3 _position;
 
 
     private void Start()
     {
       _pinManager = transform.parent.GetComponentInParent<PinManager>();
-      _position = GetComponent<RectTransform>().anchoredPosition;
+      /*var pos = GetComponent<RectTransform>().position;
+      _position = new Vector3(pos.x, pos.y, 1);*/
+      GetPoint();
     }
 
     protected override void ButtonClicked()
     {
       base.ButtonClicked();
-      _pinManager.SetPinCharacter(_value, transform.position);
+      _pinManager.SetPinCharacter(_value, _position);
       SetCanHover(false);
     }
 
+
+    void GetPoint()
+    {
+      var rectT = transform.parent.GetComponent<RectTransform>();
+      RectTransformUtility.ScreenPointToWorldPointInRectangle(
+        rectT, RectTransformUtility.WorldToScreenPoint(null,transform.position), null,
+        out var point);
+      _position = new Vector3(point.x, point.y, 0);
+    }
+    /*void GetPoint () {
+      //the new bits:
+      Rect r = RectTransformToScreenSpace(GetComponent<RectTransform>());
+      Vector3 v3 = new Vector3(r.xMin, r.yMin, 1);
+
+      //more or less original code:
+      Vector3 val = Camera.main.ScreenToWorldPoint(v3);
+      val = transform.InverseTransformPoint(val) * -1;
+      _position = new Vector3(val.x, val.y, val.z);
+    }
+
+//helper function:
+    public static Rect RectTransformToScreenSpace(RectTransform transform) {
+      Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
+      Rect rect = new Rect(transform.position.x, transform.position.y, size.x, size.y);
+      rect.x -= (transform.pivot.x * size.x);
+      rect.y -= ((1.0f - transform.pivot.y) * size.y);
+      return rect;
+    }*/
+    
     public void Clear()
     {
       DeactivateButton();
--- Prototype/Assets/Scripts/Managers/PinManager.cs
+++ Prototype/Assets/Scripts/Managers/PinManager.cs
@@ -20,10 +20,12 @@ namespace Managers
         [SerializeField] private LineRenderer lineRenderer;
         [SerializeField] private MainMenuManager mainMenuManager;
         private List<PasswordButton> _pinButtons = new List<PasswordButton>();
+        private List<Vector3> _transforms = new List<Vector3>();
 
         private void Start()
         {
             _pinButtons.AddRange(GetComponentsInChildren<PasswordButton>());
+            res = Screen.currentResolution;
         }
 
         public void ClearPin()
@@ -33,27 +35,50 @@ namespace Managers
                 btn.Clear();
             }
             pinIntegers.Clear();
+            lineRenderer.positionCount = 0;
+            
         }
 
-        public void SetPinCharacter(int value, Vector3 worldPos)
+        public void SetPinCharacter(int value, Vector3 anchoredPos)
         {
-            _currentIndex++;
-            pinIntegers.Add(value);
-            if (_currentIndex > 1)
+           
+            if (_currentIndex >= 0)
             {
                 lineRenderer.gameObject.SetActive(true);
                 var positionIndex = _currentIndex + 1;
                 lineRenderer.positionCount = positionIndex;
-                lineRenderer.SetPosition(_currentIndex, worldPos);
+                _transforms.Add(anchoredPos);
+                var correctedTransform = new Vector3(anchoredPos.x-Screen.width/2, anchoredPos.y-Screen.height/2, Screen.height); //Don't ask, I don't know
+                lineRenderer.SetPosition(_currentIndex, correctedTransform);
+                lineRenderer.widthMultiplier = 50.0f;
             }
-            else
-            {
-                lineRenderer.positionCount = 1;
-                lineRenderer.SetPosition(0, worldPos);
-            }
+            _currentIndex++;
+            pinIntegers.Add(value);
+        }
         
+        Resolution res;
+        
+        // Update is called once per frame
+        void Update () {
+     
+            if (res.width != Screen.width || res.height != Screen.height) {
+         
+                CalculateNewLinePositions();
+                res = Screen.currentResolution;
+            }
+ 
         }
 
+        void CalculateNewLinePositions()
+        {
+            for (var index = 0; index < _transforms.Count; index++)
+            {
+                var t = _transforms[index];
+                var correctedT = new Vector3(t.x-Screen.width/2, t.y-Screen.height/2, Screen.height);
+                lineRenderer.SetPosition(index, correctedT);
+            }
+        }
+        
         public void SetPin()
         {
             _pin = pinIntegers.Select(i => i.ToString()).Aggregate((i, j) => i + j);
